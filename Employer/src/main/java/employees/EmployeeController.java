2d4eea12edadec704aba4976c8fdc0baae637b43
noble.saji <noble.saji@CPU-1382.speridian.com>
2019-08-28 20:17:14 +0530

    post method to filter request

--- Employer/src/main/java/employees/EmployeeController.java
+++ Employer/src/main/java/employees/EmployeeController.java
@@ -2,6 +2,7 @@ package employees;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-	@RequestMapping("/employee")
+	@RequestMapping(value="/employee",method=RequestMethod.GET)
 	public Employee greeting(@RequestParam(value="name", defaultValue="unknown") String name) {
 		//return new Employee(counter.incrementAndGet(),String.format(template, name));
 		return new Employee("noble","m",String.format(template, name));
 	}
+    @RequestMapping(method = RequestMethod.POST)
+
+    String post() {
+
+        return "Hello from post";
+
+    }
 }
 
