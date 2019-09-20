c79fd2f2c810bf2a7cdfdde388ebfcd04008a418
noble.saji <noble.saji@CPU-1382.speridian.com>
2019-09-20 17:27:08 +0530

    marshalling 1

---
+++ Employer/src/main/java/employees/EmployeeMarshaller.java
@@ -0,0 +1,8 @@
+package employees;
+
+public class EmployeeMarshaller {
+	private Greeting greeting=new Greeting(1234,"hello");
+	private Marshaller marshaller;
+	private Unmarshaller unmarshaller;
+		
+}
