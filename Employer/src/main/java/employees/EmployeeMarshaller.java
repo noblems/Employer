81008445a5f2202add06b9235c38f7450be1f616
noble.saji <noble.saji@CPU-1382.speridian.com>
2019-09-23 19:03:07 +0530

    marshaller error

--- Employer/src/main/java/employees/EmployeeMarshaller.java
+++ Employer/src/main/java/employees/EmployeeMarshaller.java
@@ -1,8 +1,34 @@
 package employees;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.xml.transform.stream.StreamResult;
+
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.Unmarshaller;
+
 public class EmployeeMarshaller {
+	String FILE_NAME = "settings.xml";
 	private Greeting greeting=new Greeting(1234,"hello");
 	private Marshaller marshaller;
 	private Unmarshaller unmarshaller;
-		
+	
+	public void setMarshaller(Marshaller marshaller) {
+		this.marshaller=marshaller;
+	}
+	public void setUnmarshaller(Unmarshaller unmarshaller) {
+		this.unmarshaller=unmarshaller;
+	}
+	
+	public FileOutputStream returnXml() throws IOException {
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(FILE_NAME);
+            this.marshaller.marshal(greeting, new StreamResult(os));
+        } finally {
+           
+           return os;
+           
+        }
+    }
 }
--- Employer/src/main/java/employees/GreetingController.java
+++ Employer/src/main/java/employees/GreetingController.java
@@ -1,6 +1,13 @@
 package employees;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +19,12 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public FileOutputStream greeting(@RequestParam(value="name", defaultValue="World") String name) throws IOException {
+    	Resource r=new ClassPathResource("./applicationContext.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+    	EmployeeMarshaller em=(EmployeeMarshaller) factory.getBean("application");
+       // return new Greeting(counter.incrementAndGet(),
+                //            String.format(template, name));
+        return em.returnXml();
     }
 }
