8ceb7b40182b7ce227b70a3a1e8f7bfb54b75f6f
noble.saji <noble.saji@CPU-1382.speridian.com>
2019-10-04 11:49:29 +0530

    mybatis without test

--- Employer/src/main/java/employees/GreetingController.java
+++ Employer/src/main/java/employees/GreetingController.java
@@ -13,15 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import domain.Users;
+import employees.service.UserService;
+
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private UserService userService;
 
     @RequestMapping(value="/greeting",produces=MediaType.APPLICATION_XML_VALUE)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) throws IOException {
     	
         return new Greeting(counter.incrementAndGet(),String.format(template, name));
     }
+    @RequestMapping(value="/insert",produces=MediaType.APPLICATION_XML_VALUE)
+    public Greeting insert(@RequestParam(value="name", defaultValue="done") String name) throws IOException {
+    	userService = new UserService();
+    	Users user = new Users();
+	     user.setEmailId("test_email_"+System.currentTimeMillis()+"@gmail.com");
+	     user.setPassword("secret");
+	     user.setFirstName("TestFirstName");
+	     user.setLastName("TestLastName");
+	 
+	     userService.insertUser(user);
+        return new Greeting(counter.incrementAndGet(),String.format(template, name));
+    }
+    
 }
